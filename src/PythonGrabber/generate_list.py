4f2182750600465ae225a103232698536ecaa4a1
KuznetsovMikhail <KuznetsovMikhail>
2022-06-05 20:38:19 +0300

    Well???

--- src/PythonGrabber/generate_list.py
+++ src/PythonGrabber/generate_list.py
@@ -60,10 +60,10 @@ with open('Source/Oxxxymiron.txt', 'a+', encoding='utf-8') as ouf:
                             r'<i>|</i>|</a>|<br>', '',
                             re.sub(r'<a.*?>', '',
                             re.findall(
-                            r'<div class="lyrics">'
-                            + r'\n.*\n.*<!--sse-->.+<!--/sse-->',
-                            data, re.S)[0],
-                            flags=re.S)), flags=re.S), flags=re.S)))
+                                r'<div class="lyrics">'
+                                + r'\n.*\n.*<!--sse-->.+<!--/sse-->',
+                                data, re.S)[0],
+                                flags=re.S)), flags=re.S), flags=re.S)))
 
             album += text + '\n#\n'
             # print(text)
