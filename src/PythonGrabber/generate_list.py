051c0547e9c64165549f8e327e6d1fe5992724fe
KuznetsovMikhail <KuznetsovMikhail>
2022-06-05 20:22:23 +0300

    fix pep8

--- src/PythonGrabber/generate_list.py
+++ src/PythonGrabber/generate_list.py
@@ -25,13 +25,14 @@ with open('Source/Oxxxymiron.txt', 'a+', encoding='utf-8') as ouf:
         # The assembled request
         request = urllib.request.Request(album_url, None, headers)
         
-        # The data u need  
+        # The data u need
         response = urllib.request.urlopen(request)
         data = response.read().decode('utf-8')
 
         # print(data)
 
-        songs = [re.search(r'https://genius.com/.*?"',
+        songs = [re.search(
+            r'https://genius.com/.*?"',
             i).group()[:-1] for i in re.findall(
             r'href="https://genius.com/.*" class="u-display_block">'
             + r'.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n\s+\n', data)]
@@ -44,21 +45,21 @@ with open('Source/Oxxxymiron.txt', 'a+', encoding='utf-8') as ouf:
 
         for song_url in songs:
             # The assembled request
-            request = urllib.request.Request(song_url, None, headers)  
+            request = urllib.request.Request(song_url, None, headers)
             response = urllib.request.urlopen(request)
 
             # The data u need
             data = response.read().decode('utf-8')
 
-            text = re.sub(r'\n\s*\r', '', re.sub(r'\[.+\]\n', '', 
+            text = re.sub(
+                r'\n\s*\r', '', re.sub(r'\[.+\]\n', '',
                 re.sub(r'</p>.+', '', re.sub(r'.+<p>', '',
-                re.sub(r'<i>|</i>|</a>|<br>', '', re.sub(r'<a.*?>', '', 
-                re.findall(r'<div class="lyrics">' 
-                + r'\n.*\n.*<!--sse-->.+<!--/sse-->', 
+                re.sub(r'<i>|</i>|</a>|<br>', '', re.sub(r'<a.*?>', '',
+                re.findall(r'<div class="lyrics">'
+                + r'\n.*\n.*<!--sse-->.+<!--/sse-->',
                 data, re.S)[0], flags=re.S)), flags=re.S), flags=re.S)))
             
             album += text + '\n#\n'
             # print(text)
     
         ouf.write(album)
-        
