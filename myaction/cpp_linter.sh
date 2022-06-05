c4018f96abaa2cf89512d59bc4cc7eb8d496fe55
KuznetsovMikhail <46626154+KuznetsovMikhail@users.noreply.github.com>
2022-06-05 17:44:02 +0300

    Create cpp_linter.sh

---
+++ myaction/cpp_linter.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+folders=$()
+echo "${folders}"
+
+for folder in $folders; do # Not recommended, will break on whitespace
+    echo "${folder}"
+      if [[ -d ${folder} ]]; then
+        command="cpplint --recursive --linelength=120 --exclude=*/test.cpp --filter=-legal/copyright,-build/include_subdir,-runtime/references,-runtime/threadsafe_fn ${folder}/*"
+          echo "${command}"
+          if ${command} | grep "error"; then
+            echo "::error ::Review your code"
+            exit 1
+          fi
+      fi
+done
